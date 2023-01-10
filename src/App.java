70b7c2dd06d2e0abced729ee3488e50fd8dbd148
Guilherme <guialvest@gmail.com>
2023-01-09 21:56:22 -0300

    fix: nomes de variáveis

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) {
         int num = 5;
-        int maxValue = 10;
+        int tableValue = 10;
 
         System.out.printf("Tabela de multiplicação de %d\n", num);
 
-        for (int i = 0; i <= maxValue; i++) {
+        for (int i = 0; i <= tableValue; i++) {
             System.out.printf("%d x %d = %d \n", num, i, num * i);
         }
     }
